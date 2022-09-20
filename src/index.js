be28917a3a28d4e6b0ba92ace0ca6ca4cd8e7cc1
hilatd <hilat.doron@gmail.com>
2022-09-20 13:32:59 +0300

    test api from TMDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const axios = require('axios').default;
+const api_key = `ac505a02032a33d65dd28b41f72182e1`;
+const lang = 'en-US'
 
 const sayHey = (name) => {
     return `Hey ${name}`
 }
 
+async function getMovieActors(movieId) {
+    let response = [];
+    try {
+      response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${api_key}&language=${lang}`);
+      response = response?.data?.cast;
+    } catch (error) {
+      console.error(error);
+    }
+    return response;
+  }
+
 module.exports = {
-    sayHey
+    sayHey,
+    getMovieActors
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,12 @@
-const { sayHey } = require('../src')
+const { sayHey, getMovieActors } = require('../src')
+const { movies } = require('../dataForQuestions')
 
 describe('sayHey', () => {
     it('should say Hey to given name', () => {
         expect(sayHey('John')).toBe('Hey John')
+    })
+    it('should get movie cast', async() => {
+        let res = await getMovieActors(movies['Iron Man']);
+        expect(res).toHaveLength(64)
     })
 })
